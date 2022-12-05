2caecf896677b1190e3a382567ab182835bde8d7
Luigi Ballabio <luigi.ballabio@gmail.com>
2022-12-05 09:02:13 +0100

    Remove workaround

--- before_all_linux.sh
+++ before_all_linux.sh
@@ -4,13 +4,11 @@
 curl -O -L https://boostorg.jfrog.io/artifactory/main/release/1.80.0/source/boost_1_80_0.tar.gz
 tar xfz boost_1_80_0.tar.gz
 cd boost_1_80_0
-# this fails the configure check and avoids compiling the test suite
-rm boost/test/included/unit_test.hpp
 mv boost /usr/local/include/
 cd ..
 
 # Build QuantLib
 cd QuantLib-1.*/
-./configure --disable-static --enable-unity-build CXXFLAGS="-O3 -g0"
+./configure --disable-static --disable-test-suite --enable-unity-build CXXFLAGS="-O3 -g0"
 make -j 4
 cd ..
