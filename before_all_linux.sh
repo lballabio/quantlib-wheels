a8f4481426736a2f952e898662fdbe7fa03ab556
Luigi Ballabio <luigi.ballabio@gmail.com>
2022-12-21 18:42:46 +0100

    Upgrade Boost version

--- before_all_linux.sh
+++ before_all_linux.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
+set -e
 
 # Setup Boost
-curl -O -L https://boostorg.jfrog.io/artifactory/main/release/1.80.0/source/boost_1_80_0.tar.gz
-tar xfz boost_1_80_0.tar.gz
-cd boost_1_80_0
+curl -O -L https://boostorg.jfrog.io/artifactory/main/release/1.81.0/source/boost_1_81_0.tar.gz
+tar xfz boost_*.tar.gz
+cd boost_*/
 mv boost /usr/local/include/
 cd ..
 
 # Build QuantLib
 cd QuantLib-1.*/
 ./configure --disable-static --disable-test-suite --enable-unity-build CXXFLAGS="-O3 -g0"
-make -j 4
+make -j 4 install
 cd ..
