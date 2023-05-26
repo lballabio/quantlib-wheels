d3f4c1011ea710fec26a3074aff45a9bef53e247
Luigi Ballabio <luigi.ballabio@gmail.com>
2023-05-26 13:09:13 +0200

    Add jobs for aarch64 wheels

--- before_all_linux.sh
+++ before_all_linux.sh
@@ -10,6 +10,6 @@ cd ..
 
 # Build QuantLib
 cd QuantLib-1.*/
-./configure --disable-static --disable-test-suite --enable-unity-build CXXFLAGS="-O3 -g0"
+./configure --disable-static --disable-test-suite --enable-unity-build CXXFLAGS="${CXXQLFLAGS}"
 make -j 4 install
 cd ..
